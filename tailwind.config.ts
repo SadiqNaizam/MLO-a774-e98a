394d0e33770490790e7f4b00ff5302e0253563f6
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-05 18:53:39 +0530

    Files generated: tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -64,10 +64,13 @@ export default {
 				}
 			},
 			borderRadius: {
-				lg: 'var(--radius)',
-				md: 'calc(var(--radius) - 2px)',
-				sm: 'calc(var(--radius) - 4px)'
+				lg: 'var(--radius)', /* 12px */
+				md: 'calc(var(--radius) - 4px)', /* 8px */
+				sm: 'calc(var(--radius) - 8px)' /* 4px */
 			},
+			fontFamily: {
+                sans: ['var(--font-sans)'],
+            },
 			keyframes: {
 				'accordion-down': {
 					from: {
